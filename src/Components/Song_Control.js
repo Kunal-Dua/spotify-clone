2732a2c0930ff69216c7fb4a338ba4daa397f3b6
Kunal Dua <kunaldua2000@gmail.com>
2022-07-16 15:47:35 +0530

    song control

--- src/Components/Song_Control.js
+++ src/Components/Song_Control.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './CSS/Song_Control.css'
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import FavoriteIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
-const SongControl = () => {
+const Song_Control = () => {
   return (
-    <div>SongControl</div>
+    <div className='SongControl'>
+      <PlayCircleFilledIcon className='PlayCircleFilledIcon'/>
+      <FavoriteIcon className='FavoriteIcon'/>
+      <MoreHorizIcon className='MoreHorizIcon'/>
+    </div>
   )
 }
 
-export default SongControl
+export default Song_Control
