aaac50787cccdcaa8fbf8ef91c8f4b2b186a8294
Kunal Dua <kunaldua2000@gmail.com>
2022-07-18 15:25:08 +0530

    changed to redirect to firebase web url

--- src/Spotify API/spotify_login_url.js
+++ src/Spotify API/spotify_login_url.js
@@ -1,6 +1,6 @@
 const endPoint = "https://accounts.spotify.com/authorize";
-const redirect_uri = "http://localhost:3000"; //for loacl host
-// const redirect_uri = "https://spotify-clone-3666b.web.app";
+// const redirect_uri = "http://localhost:3000"; //for loacl host
+const redirect_uri = "https://spotify-clone-3666b.web.app";
 const client_id = 'b49adde5dde446e8ac28f4691742331a';
 const scope = [
     "user-modify-playback-state",
@@ -21,7 +21,7 @@ export const getTokenFromUrl = () => {
             let parts = item.split("=");
             initial[parts[0]] = decodeURIComponent(parts[1]);
             return initial;
-        },{});
+        }, {});
 }
 
 export const loginUrl = `${endPoint}?response_type=token&client_id=${client_id}&scope=${scope.join("%20")}&redirect_uri=${redirect_uri}&show_dialog=true`;
